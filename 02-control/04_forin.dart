53b350e6f720aef2cdc5743619c0d8903627ce82
Santiago Cabrera M <santiago.cabrera.murillo@gmail.com>
2021-03-06 14:54:34 -0500

    Version inicail

---
+++ 02-control/04_forin.dart
@@ -0,0 +1,11 @@
+main() {
+  List<String> listado = ['Batman', 'Superman', 'SuperGirl'];
+
+  for (int i = 0; i < listado.length; i++) {
+    print(listado[i]);
+  }
+
+  for (String nombre in listado) {
+    print(nombre);
+  }
+}
