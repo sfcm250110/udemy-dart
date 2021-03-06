2c33a6c9f8b3d17d78d7233a2056c102612d8dc0
Santiago Cabrera M <santiago.cabrera.murillo@gmail.com>
2021-03-06 15:06:02 -0500

    Video 34: Ciclo While

--- 02-control/04_forin.dart
+++ 02-control/04_forin.dart
@@ -1,9 +1,9 @@
 main() {
   List<String> listado = ['Batman', 'Superman', 'SuperGirl'];
 
-  for (int i = 0; i < listado.length; i++) {
-    print(listado[i]);
-  }
+  // for (int i = 0; i < listado.length; i++) {
+  //   print(listado[i]);
+  // }
 
   for (String nombre in listado) {
     print(nombre);
